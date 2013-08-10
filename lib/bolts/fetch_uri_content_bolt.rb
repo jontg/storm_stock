d28346b2430440653ffbe3c7075e5c9094b495dd
Jon T Gretarsson <jon@relateiq.com>
2013-08-10 18:48:25 +0000

    Implementing a fetching bolt, first pass

---
+++ lib/bolts/fetch_uri_content_bolt.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'uri'
+
+class FetchUriContentBolt < RedStorm::DSL::Bolt
+  on_receive(:emit => true, :ack => true, :anchor => true) do |tuple|
+    if tuple[0].is_a?(Hash) and tuple[0].key?(:uri)
+      uri = URI::parse(tuple[0][:uri])
+      res = Net::HTTP.start(uri.host, uri.port) { |http| http.get(uri.path) }
+      tuple[0][:header] = res.header
+      tuple[0][:body] = res.body
+      puts res.body
+      sleep 10
+      log.info(tuple[0])
+    end
+
+    tuple[0]
+  end
+end
