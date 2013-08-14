b0c672eea02a45c890cc9a4fbe7ad327f7e124c0
Jon T Gretarsson <jon@relateiq.com>
2013-08-14 19:22:24 -0400

    Simplify

--- lib/spouts/redis_pub_sub_spout.rb
+++ lib/spouts/redis_pub_sub_spout.rb
@@ -27,12 +27,7 @@ module RedisPubSubSpout
         @patterns.each do |pattern|
           Redis.new(:host => @host, :port => @port).psubscribe(pattern) do |on|
             on.pmessage do |pat, ch, message|
-              if self.class.reliable?
-                @q << [@i.to_s, {:pattern => pat, :message => message}, ch]
-                @i += 1
-              else
-                @q << [{:pattern => pat, :message => message}, ch]
-              end
+              publish_tuple(ch, {:pattern => pat, :message => message})
             end
           end
         end
@@ -43,15 +38,19 @@ module RedisPubSubSpout
           Thread.current.abort_on_exception = true
           Redis.new(:host => @host, :port => @port).subscribe(channel) do |on|
             on.message do |ch, message|
-              if self.class.reliable?
-                @q << [@i.to_s, {:message => message}, ch]
-                @i += 1
-              else
-                @q << [{:message => message}, ch]
-              end
+              publish_tuple(ch, {:message => message})
             end
           end
         end
+      end
+    end
+
+    def publish_tuple(channel, message)
+      if self.class.reliable?
+        @q << [@i.to_s, message, channel]
+        @i += 1
+      else
+        @q << [message, channel]
       end
     end
   end
